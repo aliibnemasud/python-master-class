4708af8ce3538ca8641d57fc8ebb1ebdf760a990
Ali Ibne Masud <mdmasudrony@gmail.com>
2024-05-31 23:06:58 +0600

    new files added

---
+++ HelloWorldProject/conditions.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you? "))
+
+if age >= 16 and age <= 65:
+    print("Go Swimming.")
+else:
+    print("Gelo.")
+
---
+++ HelloWorldProject/notin.py
@@ -0,0 +1,4 @@
+activity = input("What would you like to do? ")
+
+if "cinema" not in activity.casefold():
+    print("But I want to go to the cinema.")
