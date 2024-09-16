3998707106cb7b7328915de41883890915aba38d
Ali Ibne Masud <mdmasudrony@gmail.com>
2024-09-16 22:10:16 +0600

    artificial problem solved

---
+++ HelloWorldProject/data_stracture/binary_search.py
@@ -0,0 +1,15 @@
+numbers = [10, 23, 45, 67, 98, 99, 101, 103, 109, 113, 125]
+
+target_number = 45
+half = len(numbers) / 2
+
+print(half)
+
+print(9//2)
+
+# if target_number > numbers[half]:
+#     for i in range(len(numbers)):
+
+
+
+
